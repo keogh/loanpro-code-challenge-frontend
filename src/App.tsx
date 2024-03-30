6e0099421614eb7c053761b497c09084f56c895c
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-03-30 07:05:48 -0700

    Fix routes base path

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './domain/Login';
 import HomePage from './domain/Homepage';
 
+// process.env.NODE_ENV and process.env.PUBLIC_URL are set by Create React App
+// and then replaced with a string when building the project for dev and prod
+const basePath = process.env.NODE_ENV === 'production' ? process.env.PUBLIC_URL : '';
+
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: `${basePath}/`,
     element: <HomePage />,
   },
   {
-    path: "/login",
+    path: `${basePath}/login`,
     element: <Login />,
   },
 ]);
