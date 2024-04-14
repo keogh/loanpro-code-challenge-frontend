4781e92fc41476d1575b14d8eff6fa60647b76a2
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-04-14 12:24:53 -0700

    Button supports primary and secondary variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 
 interface Props extends  React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-
+  variant?: 'primary' | 'secondary';
 }
 
 const Button = React.forwardRef<HTMLButtonElement, Props>(
   ({
     children,
     className,
+    variant = 'primary',
     ...rest
   }, ref) => {
+
+    let variantClassName = `
+      bg-indigo-600 text-white hover:bg-indigo-500 focus-visible:outline-indigo-600
+    `;
+    if (variant === 'secondary') {
+      variantClassName = `
+        bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 
+        focus-visible:outline-gray-300
+      `;
+    }
+
     return (
       <button
         className={`
-          flex w-full justify-center rounded-md bg-indigo-600 
-          px-3 py-1.5 text-sm font-semibold leading-6 text-white 
-          shadow-sm hover:bg-indigo-500 focus-visible:outline 
+          flex w-full justify-center rounded-md  
+          px-3 py-1.5 text-sm font-semibold leading-6  
+          shadow-sm focus-visible:outline 
           focus-visible:outline-2 focus-visible:outline-offset-2 
-          focus-visible:outline-indigo-600
+          ${variantClassName}
           ${className}
         `}
         {...rest}
