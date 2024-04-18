7caf1c224d1c32e9144b3e6bffbb54c162bf2591
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-04-18 07:54:01 -0700

    Fix pagination pages

--- src/domain/Pagination/Pagination.tsx
+++ src/domain/Pagination/Pagination.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import {ChevronLeftIcon, ChevronRightIcon} from "@heroicons/react/20/solid";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 type Props = {
   page: number;
@@ -70,26 +70,34 @@ const Pagination = ({
 
         <div>
           <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-            <a
-              href={isFirstPage ? "#" : `/records?page=${page-1}&per_page=${perPage}`}
-              className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            <Link
+              to={`/records?page=${page-1}&per_page=${perPage}`}
+              className={`
+                ${isFirstPage ? 'pointer-events-none' : ''}
+                relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 
+                ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0
+              `}
             >
               <span className="sr-only">Previous</span>
               <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </Link>
             <span
               aria-current="page"
               className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
             >
               {page}
             </span>
-            <a
-              href={isLastPage ? "#" : `/records?page=${page+1}&per_page=${perPage}`}
-              className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+            <Link
+              to={isLastPage ? "#" : `/records?page=${page+1}&per_page=${perPage}`}
+              className={`
+                ${isLastPage ? 'pointer-events-none' : ''}
+                relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 
+                ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0
+              `}
             >
               <span className="sr-only">Next</span>
               <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
+            </Link>
           </nav>
         </div>
       </div>
