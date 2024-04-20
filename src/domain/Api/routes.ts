40e474f2f482d1a481d615cf508f90ae937e3643
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-04-19 18:32:15 -0700

    Add production base_url for api routes

--- src/domain/Api/routes.ts
+++ src/domain/Api/routes.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8000';
+export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000' : 'https://loanpro-django-api.apps.bitacoraml.com';
 
 export const SING_IN_URL = `${BASE_URL}/api/v1/sign-in`;
 export const OPERATIONS_URL = `${BASE_URL}/api/v1/operations`;
